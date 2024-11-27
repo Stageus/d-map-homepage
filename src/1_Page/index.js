fa12bfe92bac0e500c59a8acafc0126fc52d1142
RyuDongHo <thffhahs03@gmail.com>
2024-11-28 02:26:29 +0900

    chore: page 라우터 적용

--- src/1_Page/index.js
+++ src/1_Page/index.js
@@ -1,9 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Sns from "./Sns";
+import Profile from "./Profile";
+import Login from "./Login";
+import Search from "./Search";
+import Setting from "./Setting";
+import Tracking from "./Tracking";
 const Page = () => {
   return (
     <div>
-      Page
+      <Routes>
+        <Route path="/" element={<Navigate to={"/sns/:category/:userIdx"} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile/:userIdx" element={<Profile />} />
+        <Route path="/search/:category?text=" element={<Search />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/sns/:category/:userIdx" element={<Sns />} />
+        <Route path="/tracking" element={<Tracking />} />
+        {/* <Route path="*" element={} /> 에러 페이지 추가 예정 */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default Page;
