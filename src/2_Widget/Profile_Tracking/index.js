89ed01676cb8550633f9b86d7763dead22bec7da
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-12-17 02:29:38 +0900

    feat(shared) : cookie 구현

---
+++ src/2_Widget/Profile_Tracking/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { GoogleMap, LoadScript, Polyline } from "@react-google-maps/api";
+const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
+const Tracking = (props) => {
+  const { data } = props;
+  const { zoom, center, line } = data;
+
+  const polylineOptions = {
+    strokeColor: "#FF0000", // 빨간색 선
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+  };
+
+  return (
+    <>
+      <LoadScript googleMapsApiKey={API_KEY}>
+        <GoogleMap
+          mapContainerStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          center={center}
+          zoom={zoom}
+          options={{
+            heading: 150, // 지도 회전 각도 설정 (0 ~ 360)
+            disableDefaultUI: true, // UI 요소 비활성화
+          }}>
+          {/* 선 그리기 */}
+          {line.map((elem) => {
+            return <Polyline path={elem} options={polylineOptions} />;
+          })}
+        </GoogleMap>
+      </LoadScript>
+    </>
+  );
+};
+
+export default Tracking;
---
+++ src/2_Widget/Profile_Tracking/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const STYLE = {
+  Tracking: styled.div`
+    width: 100%;
+    min-height: 300px;
+    flex-grow: 1;
+  `,
+};
+
+export default STYLE;
