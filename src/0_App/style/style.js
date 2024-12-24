51f165f14deca057adb8da7d5107f4672a5bca3d
RyuDongHo <thffhahs03@gmail.com>
2024-12-25 07:09:00 +0900

    feat: global-style global-style 파일 분할과 footer디자인 수정

--- src/0_App/style/style.js
+++ src/0_App/style/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const STYLE = {
   Main: styled.main`
-  border: 1px red solid;
+    border: 1px red solid;
     width: 100%;
     height: 100%;
     padding-bottom: 32px;
