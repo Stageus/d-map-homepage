4ca638b7259f8db5c07fd68a967e8812481295ba
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-12-26 21:30:47 +0900

    feat(Search) : 기본 검색 페이지 구현

--- src/1_Page/Search/index.js
+++ src/1_Page/Search/index.js
@@ -1,9 +1,21 @@
-const Search = () => {
-  return (
-    <div>
-      Search
-    </div>
-  );
-}
+import React from "react";
+import { STYLE } from "./style";
 
-export default Search;
+const SearchBar = () => {
+  return (
+    <STYLE.Container>
+      <STYLE.InputContainer>
+        <STYLE.Input placeholder="검색할 내용을 입력하세요" />
+        <STYLE.Icon>🔍</STYLE.Icon>
+      </STYLE.InputContainer>
+      <STYLE.List>
+        <STYLE.ListItem>류동호</STYLE.ListItem>
+        <STYLE.ListItem>김연호</STYLE.ListItem>
+        <STYLE.ListItem>속초</STYLE.ListItem>
+        <STYLE.ListItem>인천</STYLE.ListItem>
+      </STYLE.List>
+    </STYLE.Container>
+  );
+};
+
+export default SearchBar;
---
+++ src/1_Page/Search/style.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const STYLE = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    margin: 0 auto;
+    font-family: "Arial", sans-serif;
+  `,
+  InputContainer: styled.div`
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+    padding: 8px 0;
+    margin-bottom: 16px;
+  `,
+  Input: styled.input`
+    flex: 1;
+    border: none;
+    outline: none;
+    font-size: 16px;
+  `,
+  Icon: styled.span`
+    margin-left: 8px;
+    font-size: 16px;
+    cursor: pointer;
+  `,
+  List: styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  `,
+  ListItem: styled.li`
+    font-size: 14px;
+    margin-bottom: 8px;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  `,
+};
