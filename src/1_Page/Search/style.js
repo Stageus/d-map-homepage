37aaa613fb94936664ed8c68b7f83ac3336bdf41
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-12-27 07:15:05 +0900

    refactor(Search) : 검색창 테마 적용 및 불필요 함수 제거

--- src/1_Page/Search/style.js
+++ src/1_Page/Search/style.js
@@ -7,19 +7,27 @@ const STYLE = {
     width: 100%;
     margin: 0 auto;
     padding: 10px;
+    background-color: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.text};
   `,
 
   List: styled.ul`
     list-style: none;
     padding: 0;
     margin: 0;
+    background-color: ${({ theme }) => theme.background};
+    border-radius: 5px;
   `,
   ListItem: styled.li`
     font-size: 14px;
     margin-bottom: 8px;
-    color: #bbbbbb;
+    color: ${({ theme }) => theme.gray};
     padding: 3px;
     cursor: pointer;
+    &:hover {
+      background-color: ${({ theme }) => theme.light_blue};
+      color: ${({ theme }) => theme.white};
+    }
   `,
 };
 
