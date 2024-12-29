c02ac990b489580911a8ea6a2714009ab8344754
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-12-30 02:51:13 +0900

    feat : setting Mode  enum 형식으로 data 수정 안전성 향상

--- src/1_Page/Profile/model/useSettingMode.js
+++ src/1_Page/Profile/model/useSettingMode.js
@@ -1,13 +1,26 @@
 import { useState } from "react";
 
+const SettingMode = Object.freeze({
+  SHARE: "공유",
+  EDIT: "수정",
+});
+
 const useSettingMode = () => {
   const [modifyMode, setIsModifyState] = useState(null);
-  const handleSetMode = (type) => setIsModifyState(type);
+
+  const handleSetMode = (type) => {
+    if (Object.values(SettingMode).includes(type)) {
+      setIsModifyState(type);
+    }
+  };
+
   const handleCloseMode = () => setIsModifyState(null);
+
   return {
     modifyMode,
     handleSetMode,
     handleCloseMode,
   };
 };
+
 export default useSettingMode;
