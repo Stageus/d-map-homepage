ba83bcb45b8095206b7084b0c678ab133e5c5ec6
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-12-30 02:53:07 +0900

    refacotr : settingMode enum 스타일 수정 및 변수명 간결화

--- src/1_Page/Profile/model/useSettingMode.js
+++ src/1_Page/Profile/model/useSettingMode.js
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
-const SettingMode = Object.freeze({
-  SHARE: "공유",
-  EDIT: "수정",
-});
+const SETTING_MODE_LIST = ["공유", "수정"];
 
 const useSettingMode = () => {
   const [modifyMode, setIsModifyState] = useState(null);
 
   const handleSetMode = (type) => {
-    if (Object.values(SettingMode).includes(type)) {
+    if (SETTING_MODE_LIST.includes(type)) {
       setIsModifyState(type);
     }
   };
