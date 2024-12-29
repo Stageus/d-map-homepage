6d75806a9bb385264a35d420d1b53ab184526af1
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-12-30 02:45:55 +0900

    style : modal open close를 위해서 함수 명명 한것 주석 추가 주석 추가로 refactor 불필요 명시 및 open 함수와 close 함수 필요성 알림

--- src/1_Page/Profile/ui/Header/model/useModifyImageModal.js
+++ src/1_Page/Profile/ui/Header/model/useModifyImageModal.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 const useModifyImageModal = () => {
+  // 모달 open close가 필요한 경우를 위해 명시적으로 함수명 설정
   const [modifyImageModal, setModifyImageModal] = useState(false);
   const handleImageModalOpen = () => setModifyImageModal(true);
   const handleImageModalClose = () => setModifyImageModal(false);
