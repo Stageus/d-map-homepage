c084830c9de147a57a002d09963ad2fc234fa287
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-01-09 18:03:14 +0900

    refactor : api 수정 및 manage 함수 명 수정

--- src/1_Page/Profile/model/useManageTrackData.js
+++ src/1_Page/Profile/model/useManageTrackData.js
@@ -7,16 +7,20 @@ const useManageTrackData = (userIdx) => {
   const token = null;
   const { track, trackLoading, trackError, fetchTrackData } =
     useGetTrackData(userIdx); // 데이터 호출 api
+
   const { modifySharing } = useModifySharingTracking(token); // 데이터 수정 api
   const { deleteTrackingImage, status } = useDeleteTrackingImage(token);
 
   const [trackData, setTrackData] = useState([]);
-  const [modifyIdxList, setModifyList] = useState([]);
 
+  // fetch된 데이터로 화면 생성
   useEffect(() => {
     if (!track) return;
     setTrackData(track);
   }, [track]);
+
+  // 수정 및 삭제 시 선택된 데이터 관리 STATE
+  const [modifyIdxList, setModifyList] = useState([]);
 
   // 데이터 변경 취소
   const handleSelectCancel = () => {
@@ -24,6 +28,7 @@ const useManageTrackData = (userIdx) => {
     setModifyList([]);
   };
 
+  // 수정 또는 삭제 시 STATE에 데이터 추가
   const handleAddModifyList = (track) => {
     if (modifyIdxList.includes(track.idx)) {
       setModifyList((prev) => prev.filter((idx) => idx !== track.idx));
@@ -32,16 +37,19 @@ const useManageTrackData = (userIdx) => {
     setModifyList((prev) => [...prev, track.idx]);
   };
 
+  // 삭제상태 트랙 선택시 -> 삭제할 데이터 추가
   const handleDeleteAdd = (track) => {
     handleAddModifyList(track);
   };
 
+  // 삭제버튼 ->  삭제 및 reFetch
   const handleDeleteTrack = async () => {
     await deleteTrackingImage(modifyIdxList);
     setModifyList([]);
     await fetchTrackData();
   };
 
+  // 데이터 상태 바꾸기 , 수정 데이터 추가
   const handleToggleTrackType = (track) => {
     handleAddModifyList(track);
     setTrackData((prevData) =>
@@ -51,6 +59,7 @@ const useManageTrackData = (userIdx) => {
     );
   };
 
+  // 수정버튼 클릭 -> 수정 및 refetch
   const handleModifyTrack = async () => {
     await modifySharing(modifyIdxList);
     setModifyList([]);
