fc57d2f7bb06386b88437e3814d4bb420c745d3d
RyuDongHo <thffhahs03@gmail.com>
2025-01-16 17:45:20 +0900

    feat: sns page api sns page에서 필요한 api를 추가/수정

--- src/3_Entity/SNS/useLikeTrackingImage.js
+++ src/3_Entity/SNS/useLikeTrackingImage.js
@@ -1,18 +1,33 @@
 import React from "react";
-const BASE_URL = "SERVER URL";
+import { fetchRequest } from "../../4_Shared/util/apiUtil";
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
+const TEST_TOKEN = process.env.REACT_APP_TESTING_ACCESS_TOKEN;
 
 const useLikeTrackingImage = (trackingImageIdx) => {
   const likeTrackingImage = async () => {
-    // 1. fetch
-    //let result = TEMP_DATA;
-
-    // 2. status error handling
-    // ...
-
-    // 3. data processing
-
-    // 4. handle loading
-    console.log("like");
+    try {
+      const response = await fetchRequest(
+        "POST",
+        `${BASE_URL}/sns/like`,
+        { tracking_idx: trackingImageIdx },
+        TEST_TOKEN
+      );
+      const data = await response.json();
+      switch (response.status) {
+        case 200:
+          break;
+        case 400:
+        case 401:
+        case 403:
+        case 404:
+        case 429:
+        case 500:
+        default:
+          console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return [likeTrackingImage];
