ebcfb8c5d79122fdd74afa87455704cecdb8eddf
RyuDongHo <thffhahs03@gmail.com>
2025-01-17 17:41:32 +0900

    feat: Modal widget api apply modal widget에서 api를 사용할 수 있도록 적용

--- src/2_Widget/Modal/index.js
+++ src/2_Widget/Modal/index.js
@@ -2,21 +2,19 @@ import React from "react";
 import { GoogleMap, Polyline } from "@react-google-maps/api";
 import STYLE from "./style";
 import useHandleModal from "./model/useHandleModal";
-import useDeleteTrackingImage from "../../3_Entity/Tracking/useDeleteTrackingImage";
-import usePostTrackingImage from "../../3_Entity/Tracking/usePostTrackingImage";
-import usePutTrackingImage from "../../3_Entity/Tracking/usePutTrackingImage";
+import postTrackingImage from "../../3_Entity/Tracking/postTrackingImage";
+import putTrackingImage from "../../3_Entity/Tracking/putTrackingImage";
+import MAPTYPE from "../../4_Shared/constant/mapType";
 
 const Modal = (props) => {
   const { children, onClose, trackData } = props;
   const [newTrackingData, setNewTrackingData] = React.useState(trackData);
   const sheetRef = React.useRef();
+  const mapRef = React.useRef(null);
   const { isVisible, translateY, isDraggingRef, handleClose } = useHandleModal(
     onClose,
     sheetRef
   );
-  const [postTrackingImage] = usePostTrackingImage(newTrackingData);
-  //const putTrackingImage = usePutTrackingImage();
-  // const deleteTrackingImage = useDeleteTrackingImage();
 
   return (
     <>
@@ -28,93 +26,101 @@ const Modal = (props) => {
         $isDragging={isDraggingRef.current}
       >
         <STYLE.Handle />
-        {trackData && (
-          <STYLE.Container>
-            {/* google map */}
-            <STYLE.MapWrapper
-              onTouchStart={(e) => {
-                e.stopPropagation();
+        <STYLE.Container>
+          {/* google map */}
+          <STYLE.MapWrapper
+            onTouchStart={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <GoogleMap
+              mapContainerStyle={{
+                width: "100%",
+                height: "400px",
+              }}
+              onLoad={(map) => {
+                mapRef.current = map;
+              }}
+              options={{
+                mapTypeId: MAPTYPE[trackData.background],
+                disableDefaultUI: true,
+                zoom: trackData.zoom,
+                center: trackData.center,
+                heading: trackData.heading,
               }}
             >
-              <GoogleMap
-                mapContainerStyle={{
-                  width: "100%",
-                  height: "400px",
-                }}
-                onLoad={(map) => {
-                  if (trackData) {
-                    map.setOptions({
-                      zoom: trackData.zoom,
-                      center: trackData.center,
-                      heading: trackData.heading,
-                    });
-                  }
-                }}
-                options={{
-                  mapId: "90f87356969d889c",
-                  disableDefaultUI: true,
-                }}
-              >
-                {trackData.line?.map((elem, idx) => (
-                  <Polyline
-                    key={idx}
-                    path={elem}
-                    options={{
-                      strokeColor: newTrackingData.lineColor,
-                      strokeOpacity: 0.8,
-                      strokeWeight: newTrackingData.lineWeight,
-                    }}
-                  />
-                ))}
-              </GoogleMap>
-            </STYLE.MapWrapper>
+              {trackData.line?.map((elem, idx) => (
+                <Polyline
+                  key={idx}
+                  path={elem}
+                  options={{
+                    strokeColor: newTrackingData.color,
+                    strokeOpacity: 0.8,
+                    strokeWeight: newTrackingData.thickness,
+                  }}
+                />
+              ))}
+            </GoogleMap>
+          </STYLE.MapWrapper>
 
-            {/* slider */}
-            <STYLE.SliderContainer
-              onTouchStart={(e) => {
-                e.stopPropagation();
+          {/* slider */}
+          <STYLE.SliderContainer
+            onTouchStart={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <label htmlFor="lineWidth">선 굵기</label>
+            <STYLE.SliderModify
+              id="lineWidth"
+              type="range"
+              min="1"
+              max="10"
+              value={newTrackingData.thickness}
+              onChange={(e) => {
+                console.log(e.target.value);
+                setNewTrackingData({
+                  ...newTrackingData,
+                  thickness: Number(e.target.value),
+                });
+              }}
+            />
+          </STYLE.SliderContainer>
+          <STYLE.SliderContainer>
+            <label htmlFor="lineColor">색 선택</label>
+            <STYLE.ColorPicker
+              id="lineColor"
+              type="color"
+              value={newTrackingData.color}
+              onChange={(e) => {
+                setNewTrackingData({
+                  ...newTrackingData,
+                  color: e.target.value,
+                });
+              }}
+            />
+          </STYLE.SliderContainer>
+          <STYLE.ButtonContainer>
+            <STYLE.Button
+              onClick={() => {
+                newTrackingData.idx === -1
+                  ? postTrackingImage(newTrackingData)
+                  : putTrackingImage(newTrackingData);
+                handleClose();
               }}
             >
-              <label htmlFor="lineWidth">선 굵기</label>
-              <STYLE.SliderModify
-                id="lineWidth"
-                type="range"
-                min="1"
-                max="10"
-                value={newTrackingData.lineWeight}
-                onChange={(e) =>
-                  setNewTrackingData({ ...newTrackingData, lineWeight: Number(e.target.value) })
-                }
-              />
-            </STYLE.SliderContainer>
-            <STYLE.SliderContainer>
-              <label htmlFor="lineColor">색 선택</label>
-              <STYLE.ColorPicker
-                id="lineColor"
-                type="color"
-                value={newTrackingData.lineColor}
-                onChange={(e) => setNewTrackingData({ ...newTrackingData, lineColor: e.target.value })}
-              />
-            </STYLE.SliderContainer>
-            <STYLE.ButtonContainer>
-              <STYLE.Button
-                onClick={() => {
-                  postTrackingImage(newTrackingData);
-                  handleClose();
-                }}
-              >
-                저장하기
-              </STYLE.Button>
-              <STYLE.Button
-                onClick={() => {
-                  handleClose();
-                }}
-              >
-                공유하기
-              </STYLE.Button>
-            </STYLE.ButtonContainer>
-          </STYLE.Container>
-        )}
+              저장하기
+            </STYLE.Button>
+            <STYLE.Button
+              onClick={() => {
+                // toSharing api
+                handleClose();
+              }}
+            >
+              공유하기
+            </STYLE.Button>
+          </STYLE.ButtonContainer>
+        </STYLE.Container>
+
         {children && children({ handleClose })}
       </STYLE.Sheet>
     </>
