b8d70a9c573068b0e5432bfc96e35a3a3a846a74
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-01-19 05:17:45 +0900

    refactor : 불필요 state 삭제

--- src/1_Page/Profile/ui/Header/ui/ModifyNameModal/model/useRandomNickname.js
+++ src/1_Page/Profile/ui/Header/ui/ModifyNameModal/model/useRandomNickname.js
@@ -4,14 +4,12 @@ import getRandomNicknames from "../../../../../../../3_Entity/Account/getRandomN
 const useRandomNickname = () => {
   const [nicknames, setNicknames] = useState([]);
   const [typeText, setTypeText] = useState("현재");
-  const [randState, setRandState] = useState(0);
   const randStateRef = useRef(0); // randState의 최신 상태를 추적
 
   // 닉네임 목록을 가져오는 함수
   const fetchNicknames = useCallback(async () => {
     const randNicknames = await getRandomNicknames();
     setNicknames(randNicknames);
-    setRandState(0);
   }, []);
 
   useEffect(() => {
@@ -27,10 +25,8 @@ const useRandomNickname = () => {
       if (nextIndex >= nicknames.length) {
         fetchNicknames();
         randStateRef.current = 0;
-        setRandState(0);
       } else {
         randStateRef.current = nextIndex;
-        setRandState(nextIndex);
       }
     },
     [nicknames, fetchNicknames]
