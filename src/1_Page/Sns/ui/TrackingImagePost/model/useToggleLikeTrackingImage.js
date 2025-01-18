5c40e0f6048cb0950653eb54025a11b25efc95d7
RyuDongHo <thffhahs03@gmail.com>
2025-01-18 17:58:41 +0900

    feat: infinite scroll paging apply 기존 pull to paging 방식에서 다시 infinite scroll 방식으로 변환 및 toggleLike 함수 수정

--- src/1_Page/Sns/ui/TrackingImagePost/model/useToggleLikeTrackingImage.js
+++ src/1_Page/Sns/ui/TrackingImagePost/model/useToggleLikeTrackingImage.js
@@ -1,14 +1,12 @@
 import React from "react";
-import usePostLikeTrackingImage from "../../../../../3_Entity/SNS/usePostLikeTrackingImage";
-import useDeleteLikeTrackingImage from "../../../../../3_Entity/SNS/useDeleteLikeTrackingImage";
+import postLikeTrackingImage from "../../../../../3_Entity/SNS/postLikeTrackingImage";
+import deleteLikeTrackingImage from "../../../../../3_Entity/SNS/deleteLikeTrackingImage";
 
 const useToggleLikeTrackingImage = (trackingImageIdx, liked_by_user) => {
-  const [postLikeTrackingImage] = usePostLikeTrackingImage(trackingImageIdx);
-  const [deleteLikeTrackingImage] = useDeleteLikeTrackingImage(trackingImageIdx);
   const [like, setLike] = React.useState(liked_by_user);
 
   const toggleLikeTrackingImage = () => {
-    !like ? postLikeTrackingImage() : deleteLikeTrackingImage();
+    !like ? postLikeTrackingImage(trackingImageIdx) : deleteLikeTrackingImage(trackingImageIdx);
     setLike(!like);
   };
 
