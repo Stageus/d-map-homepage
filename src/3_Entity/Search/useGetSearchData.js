fb16e1d9dea27f066cb2ec41506c1a6f5bb0323e
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-01-19 12:09:44 +0900

    refactor : 개발용 데이터 제거

--- src/3_Entity/Search/useGetSearchData.js
+++ src/3_Entity/Search/useGetSearchData.js
@@ -2,56 +2,8 @@ import React from "react";
 import SEARCH_TYPE from "../../1_Page/Search/constant/SEARCH_TYPE";
 import { fetchRequest } from "../../4_Shared/util/apiUtil";
 const BASE_URL = process.env.REACT_APP_SERVER_URL;
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 20;
 
-const data = {
-  rows: [
-    {
-      tracking_idx: 96,
-      user_idx: 2,
-      image:
-        "https://drawingmap.s3.ap-northeast-2.amazonaws.com/1737239861308-download-1.jpg",
-      searchpoint: "서울시 종로구",
-      line: [
-        [
-          {
-            lat: 37.440582788331874,
-            lng: 126.66146387071429,
-          },
-          {
-            lat: 37.440582788331874,
-            lng: 126.66146387071429,
-          },
-        ],
-        [
-          {
-            lat: 37.440582788331874,
-            lng: 126.66146387071429,
-          },
-          {
-            lat: 37.440582788331874,
-            lng: 126.66146387071429,
-          },
-        ],
-      ],
-      zoom: 18,
-      heading: 0,
-      sharing: true,
-      likecount: 0,
-      color: "#FF0000",
-      thickness: 2,
-      background: 0,
-      createtime: "2025-01-17T03:19:13.420Z",
-      updatetime: "2025-01-17T03:19:13.420Z",
-      nickname: "허허잇3",
-      liked_by_user: false,
-      center: {
-        lat: 37.440582788331874,
-        lng: 126.66146387071429,
-      },
-    },
-  ],
-};
 const useGetSearchData = (text, page, type) => {
   const [loading, setLoading] = React.useState(true);
   const [searchData, setSearchData] = React.useState({
@@ -64,18 +16,6 @@ const useGetSearchData = (text, page, type) => {
   }));
   const fetchSearchData = async (type) => {
     let result = [];
-
-    // 테스트용
-    result = data.rows;
-    setSearchData((prev) => ({
-      ...prev,
-      [type]: [...(prev[type] || []), ...result],
-    }));
-    setHasMoreContent((prev) => ({
-      ...prev,
-      [type]: result.length >= ITEMS_PER_PAGE,
-    }));
-    return;
     try {
       const endpoint = `${BASE_URL}/search/${type}`;
       // const endpoint = `${BASE_URL}/search/${type}&page=${page}`;
@@ -86,7 +26,7 @@ const useGetSearchData = (text, page, type) => {
       // Handle response status
       switch (response.status) {
         case 200:
-          result = data.tracking_image;
+          result = data.rows;
           console.log(result, page);
           break;
         case 400:
