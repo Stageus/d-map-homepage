5a6c5abc00f36eb51b215552711f50af30d0f32c
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-01-20 11:50:58 +0900

    image modal 훅 변수명 상세화

--- src/1_Page/Profile/ui/Header/ui/ModifyImageModalContent/model/useModifyClick.js
+++ src/1_Page/Profile/ui/Header/ui/ModifyImageModalContent/model/useModifyClick.js
@@ -4,9 +4,9 @@ import putImage from "../../../../../../../3_Entity/Account/putImage";
 const useModifyClick = (
   image,
   errorMessage,
-  imagePreview,
-  imageFile,
-  handleImageChange,
+  imagePreviewURL,
+  uploadedImageFile,
+  handleProfileImageChange,
   setMessage,
   confirmModalToggle,
   handleImageConfirmModalOpen
@@ -16,24 +16,20 @@ const useModifyClick = (
   useEffect(() => {
     imageRef.current = image;
   }, [image]);
-
   const handleModifyClick = async (handleClose) => {
     if (errorMessage) {
       setMessage(errorMessage);
       return confirmModalToggle();
     }
-
-    if (imageRef.current === imagePreview) {
+    if (imageRef.current === imagePreviewURL) {
       setMessage("사진을 변경하세요");
       return confirmModalToggle();
     }
-
     try {
-      const result = await putImage(imageFile);
+      const result = await putImage(uploadedImageFile);
       if (!result) throw new Error("이미지 업로드 실패");
-
       setMessage("변경되었습니다");
-      handleImageChange(imageFile);
+      handleProfileImageChange(uploadedImageFile);
       handleImageConfirmModalOpen(handleClose);
     } catch (error) {
       setMessage(error.message || "오류가 발생했습니다.");
