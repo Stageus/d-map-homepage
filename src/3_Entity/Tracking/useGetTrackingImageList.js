aaece1a3d715abcfd08c50b8a92da59548cbfb4e
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-01-22 02:16:12 +0900

    fix : api 연결 버그 수저ㅕㅇ userInfo와 trackingImage간 데이터 연결시 버그 발생

--- src/3_Entity/Tracking/useGetTrackingImageList.js
+++ src/3_Entity/Tracking/useGetTrackingImageList.js
@@ -8,6 +8,11 @@ const TEST_TOKEN = process.env.REACT_APP_TESTING_ACCESS_TOKEN;
 const useGetTrackingImageList = (userIdx, page, sharing) => {
   const [loading, setLoading] = useState(true);
   const [trackingImageList, setTrackingImageLists] = useState([]);
+
+  useEffect(() => {
+    console.log(trackingImageList);
+  }, [trackingImageList]);
+
   const [hasMoreContent, setHasMoreContent] = useState({
     save: false,
     share: false,
@@ -24,29 +29,31 @@ const useGetTrackingImageList = (userIdx, page, sharing) => {
   const fetchTrackingImageList = async (sharing) => {
     setLoading(true);
     try {
-      const url = `${BASE_URL}/tracking/account/${userIdx}?page=${page}&category=${sharing}`;
-      const response = await fetchRequest("GET", url, null, TEST_TOKEN);
+      const endpoint = `${BASE_URL}/tracking/account/${userIdx}?page=${page}&category=${sharing}`;
+      console.log(endpoint);
+      const response = await fetchRequest("GET", endpoint, null, TEST_TOKEN);
       if (!response.ok) {
         const errorData = await response.json();
         throw new Error(`API Error: ${errorData.message}`);
       }
       const data = await response.json();
+      console.log(data);
       updateDataAndHasMoreContent(data.tracking_image, sharing);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     } finally {
       setLoading(false);
     }
   };
 
+  useEffect(() => {
+    fetchTrackingImageList(sharing);
+  }, [userIdx, page]);
+
   // 저장된 데이터 , 공유된 데이터 불러오기
   useEffect(() => {
     fetchTrackingImageList(0);
   }, []);
-
-  useEffect(() => {
-    fetchTrackingImageList(sharing);
-  }, [userIdx, page]);
 
   return {
     trackingImageList,
