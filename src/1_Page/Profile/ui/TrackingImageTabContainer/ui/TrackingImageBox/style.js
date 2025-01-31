b1b1199d537991bb2616fed3ff6d3af6d0cd73c4
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-01-31 19:30:24 +0900

    fix : 상세모달 css 버그 수정

--- src/1_Page/Profile/ui/TrackingImageTabContainer/ui/TrackingImageBox/style.js
+++ src/1_Page/Profile/ui/TrackingImageTabContainer/ui/TrackingImageBox/style.js
@@ -54,32 +54,17 @@ const STYLE = {
   `,
   ModalContent: styled.div`
     background: white;
+    position: relative;
     width: 90%;
     height: 95vh;
-    max-width: 400px;
-    overflow-y: auto;
     padding: 20px;
     border-radius: 10px;
     box-shadow: 0 10px 30px rgba(0, 0, 0, 0.2);
   `,
   CloseButton: styled.button`
-    background: none;
-    border: none;
-    font-size: 1.5rem;
     position: absolute;
-    top: 10px;
-    right: 15px;
-    cursor: pointer;
-    color: #333;
-  `,
-  OpenButton: styled.button`
-    padding: 10px 20px;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    font-size: 1rem;
-    cursor: pointer;
+    right: 0;
+    font-size: 1.5rem;
   `,
 };
 
